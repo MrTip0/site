35a6e9049c397406d54489b6b599b5326e7e2d08
Nicolò Luigi Allegris <nico@allgraf.it>
2021-07-19 18:41:04 +0200

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# My personal site
-
-This is the repository of my personal website made with react and tailwindcss
-
-## Work in progress
-
-At the moment it is stil in work in progress, and some features are missing such as Portfolio data fetching, so it is still empty
--- tipettino/src/pages/index/Portfolio.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Portfolio() {
-    return(
-        <div className="h-auto bg-secondary-200 w-min-full flex flex-col">
-            <h1 className="text-4xl text-center uppercase text-primary-500">Portofolio</h1>
-            <h1 className="mb-3 text-2xl text-secondary-500 text-center">{"Work in progress, I'm Sorry"}</h1>
-        </div>
-    )
-}
