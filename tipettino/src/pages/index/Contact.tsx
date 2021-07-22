1441e51c38c94e30377a12541fc39f7970a3c7c4
Nicolò Luigi Allegris <nico@allgraf.it>
2021-07-22 12:28:57 +0200

    made a prettier alert

--- tipettino/src/pages/index/Contact.tsx
+++ tipettino/src/pages/index/Contact.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Dialog } from "@headlessui/react";
 
 export default class Contact extends React.Component {
     constructor(props: any) {
@@ -12,7 +13,10 @@ export default class Contact extends React.Component {
         mail: "",
         message: "",
         disabled: true,
-        sent: false
+        sent: false,
+        openAlert: false,
+        alertTitle: "",
+        alertDescription: ""
     }
 
     handleChange(event: any) {
@@ -28,23 +32,37 @@ export default class Contact extends React.Component {
 
     handleSubmit(event: any) {
         if (!this.state.sent) {
+            this.setState({
+                sent: true,
+                disabled: true
+            })
             const xhttp = new XMLHttpRequest();
             var result = ""
             xhttp.open('POST', process.env.PUBLIC_URL + '/emailer.php', true)
             xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.onload = ()=> {
+            xhttp.onload = () => {
                 result = xhttp.responseText
-                alert(result)
                 if (result === 'success') {
+                    this.showMessage('Message sent succesfully', 'you can close this window')
+                } else {
+                    this.showMessage('An error has occured', 'the server has response: ' + result)
                     this.setState({
-                        sent: true,
-                        disable: true
+                        sent: false,
+                        disabled: false
                     })
                 }
             }
             xhttp.send('name=' + this.state.name + '&mail=' + this.state.mail + '&message=' + this.state.message)
         }
         event.preventDefault()
+    }
+
+    showMessage(title: string, message: string) {
+        this.setState({
+            openAlert: true,
+            alertTitle: title,
+            alertDescription: message
+        })
     }
 
     readyToSend(){
@@ -65,7 +83,25 @@ export default class Contact extends React.Component {
                     <textarea name="message" id="message" cols={30} rows={10} placeholder="Message" onChange={this.handleChange} value={this.state.message} className="bg-primary-100 rounded mb-4 w-11/12 text-lg text-center focus:ring-4 focus:ring-primary-200 shadow-lg transition-transform duration-200 transform focus:scale-110 sm:w-9/12 md:w-1/2" required/>
                     <input type="submit" value="Send" disabled={this.state.disabled} className={"bg-primary-100 rounded mb-4 h-10 w-11/12 text-lg text-center transition-transform duration-200 transform sm:w-9/12 md:w-1/2 " + (this.state.disabled ? "text-gray-500": "text-gray-900 hover:scale-110")}/>
                 </form>
+                <Alert open={this.state.openAlert} close={()=>this.setState({openAlert: false})} title={this.state.alertTitle} description={this.state.alertDescription}/>
             </div>
         )
     }
+}
+
+const Alert = (props: any) => {
+    let button = React.useRef(null);
+    return (
+        <Dialog open={props.open} onClose={()=>props.close} className="fixed z-10 inset-0 overflow-y-auto">
+                <Dialog.Overlay className="fixed inset-0 bg-secondary-50 bg-opacity-70 flex flex-row items-center">
+                    <div className="flex flex-col items-center w-screen">
+                        <div className="flex flex-col w-80 rounded bg-primary-200">
+                            <Dialog.Title className="w-auto m-5 text-xl font-semibold text-center">{props.title}</Dialog.Title>
+                            <Dialog.Description className="w-auto m-5 text-base font-semibold text-center">{props.description}</Dialog.Description>
+                            <button ref={button} className="h-8 w-auto rounded border-2 border-primary-500 m-5" onClick={props.close}>Close</button>
+                        </div>
+                    </div>
+                </Dialog.Overlay>
+        </Dialog>
+    )
 }
